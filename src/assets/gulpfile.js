86209c3f2406b42190f2eeb0d4f5c26c8ec1e2b2
STStereotype <62321778+STStereotype@users.noreply.github.com>
2022-11-08 22:18:17 +0700

    Gulp
    Добавлен Gulp в проект

---
+++ src/assets/gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+
+//SASS
+var sass = require('gulp-sass')(require('sass'));
+var sourcemaps = require('gulp-sourcemaps');
+var groupmq = require('gulp-group-css-media-queries');
+var minifyCss = require('gulp-cssnano');
+
+//util
+var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var onError = function (err) {
+  notify.onError({
+    title: 'Gulp',
+    subtitle: 'Failure!',
+    message: 'Error: <%= error.message %>',
+    sound: 'Beep'
+  })(err);
+
+  this.emit('end');
+};
+
+//SASS
+gulp.task('sass', function () {
+  return gulp.src('sass/screen.sass')
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(sass())
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('css'))
+    .pipe(notify({'title': 'Gulp-message', 'message': 'SASS compiled!', onLast: true}))
+});
+
+gulp.task('minify-css', function () {
+  return gulp.src('css/screen.css')
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(groupmq())
+    .pipe(minifyCss({discardComments: {removeAll: true}}))
+    .pipe(rename('screen.min.css'))
+    .pipe(gulp.dest('css'));
+});
+
+// Gulp production
+gulp.task('default', gulp.parallel('sass'), function () {
+  /*watchers*/
+  gulp.watch('sass/**', gulp.series('sass'));
+
+  notifier.notify({title: 'Gulp-production', message: 'without bsync+pug'});
+});
+
